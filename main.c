f1e8eb48511570521875f977d43331b1257de890
ShinHyeongSeo <21700404@handong.edu>
2020-05-26 23:18:46 +0900

    Change to linked list

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct st_minterm {
+  int dec;
+  int* bin;
+  int count;
+  struct st_minterm* right;
+} minterm;
+
 int main()
 {
   int num_variable, num_minterm, num_dont_care;
   int *minterm_list, *dont_care_list;
   int** minterm_table;
-  int i,j,k,count = 0,temp,min;
-  int index;
-
-  int* temp2;
+  minterm* temp = NULL;
+  minterm* root = (minterm*)malloc(sizeof(mitnerm));
+  int i;
 
   scanf("%d %d %d", &num_variable, &num_minterm, &num_dont_care);
 
@@ -21,84 +27,16 @@ int main()
   for(i = 0; i < num_minterm; i++)
   {
     scanf("%d",&minterm_list[i]);
+
   }
   for(i = 0; i < num_dont_care; i++)
   {
     scanf("%d", &dont_care_list[i]);
   }
 
-  minterm_table = (int**)malloc(sizeof(int*) * (num_minterm + num_dont_care));
-  for(i = 0; i < num_minterm + num_dont_care; i++)
-  {
-    minterm_table[i] = (int*)malloc(sizeof(int) * num_variable+2);
-  }
-
-  for(i = 0; i < num_minterm + num_dont_care; i++)
-  {
-    if(i < num_minterm)
-    {
-      temp = minterm_list[i];
-      for(j = num_variable+1; j > 1; j--)
-      {
-        minterm_table[i][j] = temp % 2;
-        temp /= 2;
-        if(minterm_table[i][j] == 1)
-        {
-          count++;
-        }
-      }
-      minterm_table[i][1] = minterm_list[i];
-      minterm_table[i][0] = count;
-      count = 0;
-    }
-    else {
-      index = i - num_minterm;
-      temp = dont_care_list[index];
-      for(j = num_variable+1; j > 1; j--)
-      {
-        minterm_table[i][j] = temp % 2;
-        temp /= 2;
-        if(minterm_table[i][j] == 1)
-        {
-          count++;
-        }
-      }
-      minterm_table[i][1] = dont_care_list[index];
-      minterm_table[i][0] = count;
-      count = 0;
-    }
-  }
-
-  for(i = 0; i < num_minterm + num_dont_care; i++)
-  {
-    for(j = i+1; j < num_minterm + num_dont_care; j++)
-    {
-      if(minterm_table[i][0] >= minterm_table[j][0])
-      {
-        temp2 = minterm_table[i];
-        minterm_table[i] = minterm_table[j];
-        minterm_table[j] = temp2;
-
-      }
-    }
-  }
- /
-  for(i = 0; i < num_minterm + num_dont_care; i++)
-  {
-    for(j = 0; j < num_variable+2; j++)
-    {
-      printf("%d ", minterm_table[i][j]);
-    }
-    printf("\n");
-  }
-
   free(minterm_list);
   free(dont_care_list);
-  for(i = 0; i < num_minterm + num_dont_care; i++)
-  {
-    free(minterm_table[i]);
-  }
-  free(minterm_table);
+  
 
   return 0;
 }
